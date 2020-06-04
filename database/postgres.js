2e63d425a064be91c1f6562be0acc57ee1a5d001
Ubuntu <ubuntu@ip-172-31-57-229.ec2.internal>
2020-06-04 21:39:47 +0000

    Fixes conflict in database

--- database/postgres.js
+++ database/postgres.js
@@ -3,15 +3,7 @@ const redis = require('redis');
 const addressRedis = process.env.REDIS_ADDRESS || 'redis://steamy.xmpekd.ng.0001.use1.cache.amazonaws.com:6379';
 const client = redis.createClient(addressRedis);
 
-const db = pgp('postgres://postgres:postgres@localhost:5432/steamy');
-
-db.one('SELECT $1 AS value', 'Postgres connected on port 5432')
-  .then((data) => {
-    console.log('DATA:', data.value);
-  })
-  .catch((error) => {
-    console.log('ERROR:', error);
-  });
+const db = pgp('postgres://postgres:postgres@35.153.78.250:5432/steamy');
 
 const getOne = (id, callback) => {
   db.one('SELECT game FROM games WHERE proxyid=$1', id)
