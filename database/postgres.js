f666f979ddfa36e97e7a560927844b8744f39a32
Jay Barr <jayrbarr@gmail.com>
2020-06-05 17:26:45 -0400

    Removes redis for now

--- database/postgres.js
+++ database/postgres.js
@@ -1,7 +1,7 @@
 const pgp = require('pg-promise')(/* options */);
 const redis = require('redis');
-const addressRedis = process.env.REDIS_ADDRESS || 'redis://steamy.xmpekd.ng.0001.use1.cache.amazonaws.com:6379';
-const client = redis.createClient(addressRedis);
+// const addressRedis = process.env.REDIS_ADDRESS || 'redis://steamy.xmpekd.ng.0001.use1.cache.amazonaws.com:6379';
+// const client = redis.createClient(addressRedis);
 
 const db = pgp('postgres://postgres:postgres@35.153.78.250:5432/steamy');
 
@@ -16,7 +16,7 @@ db.one('SELECT $1 AS value', 'Postgres connected on port 5432')
 const getOne = (id, callback) => {
   db.one('SELECT game FROM games WHERE proxyid=$1', id)
     .then((data) => {
-      client.set(id, JSON.stringify([data.game[0]]));
+      // client.set(id, JSON.stringify([data.game[0]]));
       callback(null, [data.game[0]]);
     })
     .catch((err) => {
